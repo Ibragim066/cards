bd8efbaabcf811242d132b8fc23e74b796560347
Ibragim <gamemega.ao@gmail.com>
2024-05-21 11:06:57 +0300

    implemented logic

--- src/App.js
+++ src/App.js
@@ -1,2 +1,20 @@
 import { nanoid } from "nanoid";
 import "./App.css";
+
+import { cards } from "./data";
+import { CardLIst } from "./components/CardList/CardLIst";
+import { Card, Collapse } from "./components";
+
+const App = () => (
+  <>
+    <CardLIst id="cards">
+      {cards.map((card) => (
+        <Card key={nanoid()} item={card} />
+      ))}
+    </CardLIst>
+
+    <Collapse collapsedLabel="Развернуть" expandedLabel="Свернуть" />
+  </>
+);
+
+export default App;
---
+++ src/data/cards.ts
@@ -0,0 +1,21 @@
+export const cards = [
+    {
+      title: "Card title",
+      url: "https://avatars.dzeninfra.ru/get-zen_doc/1593402/pub_63d9048ae905c27965ac4526_63d92667e1fddd4e29b4b1b0/scale_1200",
+      nameImg: "beautiful flower",
+      text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      link: "https://example.com/",
+    },
+    {
+      title: "Special title treatment",
+      text: "With supporting text below as a natural lead-in to additional content.",
+      link: "https://example.com/",
+    },
+    {
+      title: "Card title",
+      url: "https://vsegda-pomnim.com/uploads/posts/2022-04/1649671606_96-vsegda-pomnim-com-p-kotenok-s-tsvetami-foto-106.jpg",
+      nameImg: "beautiful flower",
+      text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      link: "https://example.com/",
+    },
+  ];
